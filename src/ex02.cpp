a40620d3cf3240009151452864fcb9dd7f3612dd
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-04-07 09:54:23 +0200

    ex02 q1

--- src/ex02.cpp
+++ src/ex02.cpp
@@ -1,3 +1,75 @@
+#include <map>
+#include <iostream>
+
+struct Position
+{
+    int x = 0;
+    int y = 0;
+};
+
+enum Direction { Haut, Droite, Bas, Gauche };
+
+std::map<Direction, Position> d_to_p = {
+    {Haut, {0,-1}},
+    {Droite, {1,0}},
+    {Bas, {0,1}},
+    {Gauche, {-1,0}}
+};
+
+///////////////////////////////////////////////////
+// Operators
+///////////////////////////////////////////////////
+
+//https://dsmte.github.io/Learn--cpp_programming/Lessons/S2/Operators
+
+bool operator==(Position const& a, Position const& b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+std::ostream& operator<<(std::ostream& os, Position const& p) {
+    os << "(" << p.x << "," << p.y << ")";
+    return os;
+}
+
+Position operator+=(Position& a, Position const& b) {
+    a.x += b.x;
+    a.y += b.y;
+    return a;
+}
+
+Position& operator+(Position& p, Direction const& d) {
+    p += d_to_p[d];
+    return p;
+}
+
+Position& operator+=(Position& p, Direction const& d) {
+    p += d_to_p[d];
+    return p;
+}
+
+///////////////////////////////////////////////////
+
+void turn_right(Direction& d) {
+    switch (d)
+    {
+    case Direction::Haut:
+        d = Direction::Droite;
+        break;
+    case Direction::Droite:
+        d = Direction::Bas;
+        break;
+    case Direction::Bas:
+        d = Direction::Gauche;
+        break;
+    case Direction::Gauche:
+        d = Direction::Haut;
+        break;
+    default:
+        break;
+    }
+}
+
 int main() {
+
     return 0;
 }
